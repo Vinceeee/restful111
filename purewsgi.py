6c3c46da4c01b5d32fc68e50c5fd3c92404da3ce
wenxingtang <491323832@qq.com>
2024-10-15 10:23:38 +0800

    update # Reviewed, transaction id: 1696

--- purewsgi.py
+++ purewsgi.py
@@ -5,4 +5,9 @@ def hello_world(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return ['Hello, World!\r\n']
 
+def info(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['Hello, info!\r\n']
+
+
 wsgi.server(eventlet.listen(('0.0.0.0', 8090)), hello_world)
